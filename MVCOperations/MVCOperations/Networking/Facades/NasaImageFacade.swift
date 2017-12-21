3b425e8a552f13a32ce6a1661a43bc640994f056
Scott <scott.mehus@possible.com>
2017-12-21 16:30:57 -0700

    One way of doing it

--- MVCOperations/MVCOperations/Networking/Facades/NasaImageFacade.swift
+++ MVCOperations/MVCOperations/Networking/Facades/NasaImageFacade.swift
@@ -14,9 +14,15 @@ class JSONProcessor {
         
     }
     
+    private let handler: ([NASAModel]) -> ()
+    
+    init(completion: @escaping ([NASAModel]) -> ()) {
+        self.handler = completion
+    }
+    
     func process() -> ((JSON?, Error?) -> ()) {
-        return { json, error in
-            
+        return { [weak self] json, error in
+            self?.handler([])
         }
     }
 }
@@ -25,13 +31,47 @@ class JSONProcessor {
 internal final class NasaImageFacade {
     
     private let session: Session
+    private let operationQueue: OperationQueue
     
-    init(session: Session) {
+    init(session: Session, queue: OperationQueue) {
+        self.session = session
+        self.operationQueue = queue
+    }
+    
+    func search(for query: String, completion: @escaping () -> ()) {
+
+        let handler: (OperationResult<[NASAModel]>) -> () = { result in
+            completion()
+        }
+        
+        let operation = SearchOperation(query: "", session: session, handler: handler)
+        operationQueue.addOperation(operation)
+    }
+}
+
+struct NASAModel {
+    
+}
+
+class SearchOperation: ObservedOperation<[NASAModel]> {
+    
+    private var searchString: String?
+    private var session: Session?
+    
+    convenience init(query: String, session: Session, handler: @escaping (OperationResult<[NASAModel]>) -> ()) {
+        self.init(handler: handler)
+        self.searchString = query
         self.session = session
     }
     
-    func search(for query: String) {
-        let processor = JSONProcessor()
+    
+    override func execute() {
+        guard let session = session, let query = searchString else { finish(data: nil); return }
+        let processor = JSONProcessor { [weak self] models in
+            self?.finish(data: models)
+        }
+        
+        
         let request = NasaRequest.images(query: query)
         session.execute(request: request, handler: processor.process())
     }
