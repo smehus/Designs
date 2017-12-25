8db3f16982a3d13380d6b25ca5add0d3b470ee6c
Scott <scott.mehus@possible.com>
2017-12-25 15:18:46 -0700

    Renaming stuff

---
+++ ReactiveMVVM/ReactiveMVVM/GatewayViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ReactiveMVVM
+//
+//  Created by scott mehus on 12/25/17.
+//  Copyright © 2017 Mehus. All rights reserved.
+//
+
+import UIKit
+
+class GatewayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
