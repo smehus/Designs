aec4a6add1b5d52d4bcee8d367ddc32b3ff9f2b1
Scott <scott.mehus@possible.com>
2017-12-25 16:13:57 -0700

    Setting up requests

--- ReactiveMVVM/ReactiveMVVM/GatewayViewController.swift
+++ ReactiveMVVM/ReactiveMVVM/GatewayViewController.swift
@@ -14,6 +14,46 @@ struct APOD {
     
 }
 
+typealias JSON = [String: Any]
+
+protocol Request {
+    var baseURL: URL { get }
+    var parameters: [String: String]? { get }
+    var httpHeaders: [String: String]? { get }
+    var httpBody: Data? { get }
+}
+
+extension Request {
+    
+    var urlRequest: URLRequest? {
+        guard var component = URLComponents(url: baseURL, resolvingAgainstBaseURL: false) else {
+            print("❌ Failed to create url components")
+            return nil
+        }
+        
+        if let params = parameters {
+            component.queryItems = params.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+        
+        guard let url = component.url else {
+            print("❌ Failed to create url")
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        
+        if let headers = httpHeaders {
+            for header in headers {
+                request.addValue(header.value, forHTTPHeaderField: header.key)
+            }
+        }
+        
+        request.httpBody = httpBody
+        
+        return request
+    }
+}
+
 enum NetworkError: Error {
     case fake
 }
@@ -26,6 +66,9 @@ class GatewayViewController: UIViewController {
     
     private func fetch() -> SignalProducer<Data?, NetworkError>? {
         return SignalProducer({ () -> Result<Data?, NetworkError> in
+            
+            
+            
             return Result.success(nil)
         })
     }
