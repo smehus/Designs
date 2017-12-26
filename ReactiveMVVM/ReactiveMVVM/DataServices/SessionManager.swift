ba6b024fc8ca1c2d4b924f6d9a79de10617f17e5
Scott <scott.mehus@possible.com>
2017-12-26 08:58:04 -0700

    Using signal

--- ReactiveMVVM/ReactiveMVVM/DataServices/SessionManager.swift
+++ ReactiveMVVM/ReactiveMVVM/DataServices/SessionManager.swift
@@ -8,8 +8,10 @@
 
 import Foundation
 import Result
+import ReactiveSwift
 
 enum NetworkError: Error {
+    case unknown
     case unrecognizedHTTPResponse
     case invalideCode
 }
@@ -23,22 +25,31 @@ internal final class SessionManager {
                 statusCodeRule(codeRange: 400...499, shouldContain: false)]
     }
     
-    func execute(request: Request) {
-        guard let urlRequest = request.urlRequest else { return }
-        let task = session.dataTask(with: urlRequest) { [weak self] (data, response, error) in
-            guard let strongSelf = self, let res = response as? HTTPURLResponse else { return }
-            
-            let responseHandler = strongSelf.rules.reduce(into: .success(nil), { (result, rule) in
-                result = rule(res)
-            })
-            
-            switch responseHandler {
-            case .failure: break
-            case .success: break
-            }
+    func execute(request: Request) -> SignalProducer<Result<Data, NetworkError>, NetworkError> {
+        return SignalProducer {[weak self] observer, disposable in
+            guard let urlRequest = request.urlRequest else { return }
+            self?.session.dataTask(with: urlRequest) { [weak self] (data, response, error) in
+                guard
+                    let strongSelf = self,
+                    let res = response as? HTTPURLResponse,
+                    let data = data
+                else {
+                    observer.send(error: NetworkError.unknown)
+                    return
+                }
+                
+                let responseHandler = strongSelf.rules.reduce(into: .success(nil), { (result, rule) in
+                    result = rule(res)
+                })
+                
+                switch responseHandler {
+                case .failure(let handlerError):
+                    observer.send(error: handlerError)
+                case .success:
+                    observer.send(value: .success(data))
+                }
+            }.resume()
         }
-        
-        task.resume()
     }
     
     func statusCodeRule(codeRange: ClosedRange<Int>, shouldContain: Bool) -> Rule {
